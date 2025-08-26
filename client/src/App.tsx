99712f4adbea322f5e7569246df4b5dd73015b59
xianfanhuang <18523689860@qq.com>
2025-08-26 15:19:28 +0800

    App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route }from "wouter/hash";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
