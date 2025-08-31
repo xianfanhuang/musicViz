234b15eba09f0e25447d8148458a3c70394242e1
xianfanhuang <18523689860@qq.com>
2025-08-31 21:56:10 +0800

    创建 particles.js

---
+++ visualizations/particles.js
@@ -0,0 +1,45 @@
+// 粒子类定义
+class Particle {
+    constructor(x, y) {
+        this.pos = createVector(x, y);
+        this.vel = p5.Vector.random2D().mult(random(1, 5) * audioData.treble * currentVisualEnergy * 2);
+        this.acc = createVector(0, 0);
+        this.lifespan = 255 * (1 - audioData.mids);
+        this.hue = baseColor[0];
+    }
+    
+    update() {
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.lifespan -= 5;
+        
+        this.size = 8 * pulseFactor;
+    }
+    
+    show() {
+        noStroke();
+        fill(this.hue, baseColor[1], baseColor[2], map(this.lifespan, 0, 255, 0, 1));
+        ellipse(this.pos.x, this.pos.y, this.size, this.size);
+    }
+    
+    isFinished() {
+        return this.lifespan < 0;
+    }
+}
+
+// 可视化函数
+visualizations.particles = function() {
+    background(0, 0, 0, 0.1); 
+    
+    if (isPlaying && random(1) < audioData.treble * 1) {
+        let newParticle = new Particle(width / 2, height / 2);
+        particles.push(newParticle);
+    }
+    
+    for (let particle of particles) {
+        particle.update();
+        particle.show();
+    }
+    
+    particles = particles.filter(p => !p.isFinished());
+}
