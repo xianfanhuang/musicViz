cec9ac19e9c855651a70eabd21b8da45972747d9
xianfanhuang <18523689860@qq.com>
2025-09-09 20:31:47 +0800

    更新 bars.js

--- visualizations/bars.js
+++ visualizations/bars.js
@@ -1,17 +1,11 @@
-// 可视化函数
-visualizations.bars = function() {
-    // Translate to top-left corner to simulate 2D canvas
-    push();
-    translate(-width / 2, -height / 2);
-
+visualizations.bars = function(audioData, dataArray) {
     background(0, 0, 0, 1);
     
-    let barWidth = width / bufferLength;
-    for (let i = 0; i < bufferLength; i++) {
+    let barWidth = width / audioManager.bufferLength;
+    for (let i = 0; i < audioManager.bufferLength; i++) {
         let barHeight = map(dataArray[i], 0, 255, 0, height);
-        let sat = map(i, 0, bufferLength, 50, 100);
+        let sat = map(i, 0, audioManager.bufferLength, 50, 100);
         fill(baseColor[0], sat, baseColor[2]);
         rect(i * barWidth, height - barHeight, barWidth, barHeight);
     }
-    pop();
 }
