39fee5f18bff4ec210121e1463c15379fc052dd2
xianfanhuang <18523689860@qq.com>
2025-09-09 20:31:27 +0800

    更新 particles.js

--- visualizations/particles.js
+++ visualizations/particles.js
@@ -1,24 +1,21 @@
-// 粒子类定义
 class Particle {
-    constructor(x, y) {
+    constructor(x, y, audioData) {
         this.pos = createVector(x, y);
-        this.vel = p5.Vector.random2D().mult(random(1, 5) * audioData.energy * 2);
+        this.vel = p5.Vector.random2D().mult(random(1, 5) * audioData.treble * currentVisualEnergy * 2);
         this.acc = createVector(0, 0);
         this.lifespan = 255 * (1 - audioData.mids);
-        this.hue = baseColor[0];
     }
     
     update() {
         this.vel.add(this.acc);
         this.pos.add(this.vel);
         this.lifespan -= 5;
-        
         this.size = 8 * pulseFactor;
     }
     
     show() {
         noStroke();
-        fill(this.hue, baseColor[1], baseColor[2], map(this.lifespan, 0, 255, 0, 1));
+        fill(baseColor[0], baseColor[1], baseColor[2], map(this.lifespan, 0, 255, 0, 1));
         ellipse(this.pos.x, this.pos.y, this.size, this.size);
     }
     
@@ -27,14 +24,14 @@ class Particle {
     }
 }
 
-// 可视化函数
-visualizations.particles = function() {
+let particles = [];
+const MAX_PARTICLES = 500;
+
+visualizations.particles = function(audioData, dataArray) {
     background(0, 0, 0, 0.1); 
     
-    // Create particles based on overall energy, making it more robust
-    if (isPlaying && random(1) < audioData.energy * 2) {
-        // In WEBGL mode, (0,0) is the center
-        let newParticle = new Particle(0, 0);
+    if (audioManager.isPlaying && random(1) < audioData.treble * 1) {
+        let newParticle = new Particle(width / 2, height / 2, audioData);
         particles.push(newParticle);
     }
     
